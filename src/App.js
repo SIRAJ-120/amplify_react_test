58ccb8d0dfc601e433c395cfc61931dc2adfa05b
SIRAJ-120 <aseemanadaf@gmail.com>
2023-09-17 15:22:54 +0530

    NEXT  UPDATE

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>WEL COME TO GOVT POLYTECHNIC VIJAPUR </h1>
       </header>
     </div>
   );
